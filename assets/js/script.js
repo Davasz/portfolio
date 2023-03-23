dde93f96146979994641f7c19148acd28663b9d5
Davi Ribeiro Souza <davidavi200615@gmail.com>
2023-03-23 18:39:44 -0300

    replace window

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// replace windows
+const ButtonRep = document.querySelector(".repositorio")
+ButtonRep.addEventListener('click', () => window.location.href = 'https://github.com/Davasz?tab=repositories')
