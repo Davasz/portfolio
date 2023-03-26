33adfb7d64ef88b032cd38525518c099ce985120
Davi Ribeiro Souza <davidavi200615@gmail.com>
2023-03-26 18:40:35 -0300

    create projects page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 // replace windows
 const ButtonRep = document.querySelector(".repositorio")
+
+const CardPokedex = document.querySelector("#pokedex")
+const CardFormove = document.querySelector("#formove")
+const CardForetec = document.querySelector("#foretec")
+const CardRating = document.querySelector("#rating-page")
+
 ButtonRep.addEventListener('click', () => window.location.href = 'https://github.com/Davasz?tab=repositories')
+
+CardPokedex.addEventListener('click', () => window.location.href = 'https://github.com/Davasz/Pokedex')
+CardFormove.addEventListener('click', () => window.location.href = 'https://github.com/Davasz/formove')
+CardForetec.addEventListener('click', () => window.location.href = 'https://github.com/Davasz/foretec')
+CardRating.addEventListener('click', () => window.location.href = 'https://github.com/Davasz/qualityrating-page')
